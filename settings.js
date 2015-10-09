93b19bfb5751b7bf9e48ef76da0c08134d7bf3ca
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-10-09 23:48:53 +0800

    Settings for users to set parameters

---
+++ settings.js
@@ -0,0 +1,35 @@
+const USERNAME = '',
+    PASSWORD = '',
+    FOLDER = 'C:\\Users\\User\\Dropbox\\courses\\',
+    COURSES = [
+        {
+            name: 'Machine Learning 2015',
+            url: 'http://edimension.sutd.edu.sg/course/view.php?id=1035'
+        },
+        {
+            name: 'Database 2015',
+            url: 'http://edimension.sutd.edu.sg/course/view.php?id=1036'
+        }
+    ];
+
+module.exports = {
+    USERNAME: USERNAME,
+    PASSWORD: PASSWORD,
+    LOGIN_URL: 'http://edimension.sutd.edu.sg/login/index.php',
+    ROOT_PATH: FOLDER,
+    COURSES: COURSES,
+    LOGINOPTS: {
+        method: 'POST',
+        url: 'http://edimension.sutd.edu.sg/login/index.php',
+        headers: {
+            'postman-token': '52d20709-92a7-afb1-0c66-682a41753aee',
+            'cache-control': 'no-cache',
+            'content-type': 'multipart/form-data; boundary=---011000010111000001101001'
+        },
+        formData: {
+            username: USERNAME,
+            password: PASSWORD
+        },
+        followAllRedirects: true
+    }
+};
