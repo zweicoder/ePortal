bf156556991954112cd0fcb4b2b69be69054362b
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-10-09 20:03:01 +0800

    Better Portal by Design

--- Portal.js
+++ Portal.js
@@ -5,28 +5,26 @@ request = request.defaults({jar: true});
 
 const LOGIN_URL = 'http://edimension.sutd.edu.sg/login/index.php',
     CREDENTIALS_PATH = 'credentials.json',
-    ROOT_PATH = 'C:\\Users\\User\\Dropbox\\Machine Learning 2015\\';
-//ROOT_PATH = 'files\\';
+//ROOT_PATH = 'C:\\Users\\User\\Dropbox\\courses\\Database 2015\\';
+    ROOT_PATH = 'files\\';
 
-var credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH));
-var catalog = JSON.parse(fs.readFileSync('eportal-catalog.json'));
-
-var formData = {
-    username: credentials.username,
-    password: credentials.password
-};
-
-var loginOpts = {
-    method: 'POST',
-    url: LOGIN_URL,
-    headers: {
-        'postman-token': '52d20709-92a7-afb1-0c66-682a41753aee',
-        'cache-control': 'no-cache',
-        'content-type': 'multipart/form-data; boundary=---011000010111000001101001'
+var credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH)),
+    catalog = JSON.parse(fs.readFileSync('eportal-catalog.json')),
+    formData = {
+        username: credentials.username,
+        password: credentials.password
     },
-    formData: formData,
-    followAllRedirects: true
-};
+    loginOpts = {
+        method: 'POST',
+        url: LOGIN_URL,
+        headers: {
+            'postman-token': '52d20709-92a7-afb1-0c66-682a41753aee',
+            'cache-control': 'no-cache',
+            'content-type': 'multipart/form-data; boundary=---011000010111000001101001'
+        },
+        formData: formData,
+        followAllRedirects: true
+    };
 
 // Gets resource (most likely PDF) link from DOM
 var getResourceUrl = function (body) {
@@ -47,19 +45,21 @@ var downloadFolder = function (folder) {
         var fileName = folderName + "\\" + sanitize(file.name);
         request(file.url, function (e, response, body) {
             var contentType = response.headers['content-type'];
+            var ext;
             // If this link is a file, write out the body
-            if (contentType && contentType.match(/document/)) {
-                var ext = response.headers['content-disposition'].match(/\.[0-9a-z]+/i)[0];
-                fileName = fileName + ext;
-                console.log("Summoning: ", fileName);
-                fs.writeFileSync(fileName, body);
-            }
-            // else we get the resource link from the DOM
-            else if (url = getResourceUrl(body)) {
-                var ext = url.match(/\.[0-9a-z]+$/i)[0];
+            if (contentType && contentType.match(/text\/html/)) {
+                var url = getResourceUrl(body);
+                ext = url.match(/\.[0-9a-z]+$/i)[0];
                 fileName = fileName + ext;
                 console.log("Summoning: ", fileName);
                 request(url).pipe(fs.createWriteStream(fileName));
+            }
+            // else we get the resource link from the DOM
+            else {
+                ext = response.headers['content-disposition'].match(/filename=".+(\.[0-9a-z]+)"/i)[1];
+                fileName = fileName + ext;
+                console.log("Summoning: ", fileName);
+                fs.writeFileSync(fileName, body);
             }
         })
     });
@@ -75,3 +75,4 @@ var openPortal = function (catalog) {
 };
 
 openPortal(catalog);
+fs.writeFileSync(ROOT_PATH + "eportal-catalog.json", catalog);
